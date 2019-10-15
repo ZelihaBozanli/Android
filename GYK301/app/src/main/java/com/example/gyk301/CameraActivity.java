654c8ab1ca0c5becf27516d845a1a67ac5317fa5
Zeliha <zelihabozanli@gmail.com>
2019-10-16 01:50:16 +0300

    degisim

---
+++ GYK301/app/src/main/java/com/example/gyk301/CameraActivity.java
@@ -0,0 +1,91 @@
+package com.example.gyk301;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.MediaController;
+import android.widget.Switch;
+import android.widget.VideoView;
+
+public class CameraActivity extends AppCompatActivity {
+    Button btnCamera;
+    Button btnVideo;
+
+    private static final int VIDEO_ACTION_CODE = 101;
+    private static final int IMAGE_ACTION_CODE = 102;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camera);
+
+        btnCamera = (Button) findViewById(R.id.btn_camera);
+        btnVideo = (Button) findViewById(R.id.btn_video);
+
+        btnCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                takePhoto();
+
+            }
+        });
+
+        btnVideo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                takeVideo();
+            }
+        });
+    }
+
+    public void takePhoto() {
+
+        Intent i = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(i, IMAGE_ACTION_CODE);
+
+    }
+
+    public void takeVideo() {
+
+        Intent i = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
+        startActivityForResult(i, VIDEO_ACTION_CODE);
+
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode != RESULT_OK) return;
+
+        switch(requestCode)
+
+        {
+            case VIDEO_ACTION_CODE:
+                VideoView videoView=((VideoView)findViewById(R.id.vv_video));
+                videoView.setVideoURI(data.getData());
+                videoView.setMediaController(new MediaController(this));
+                videoView.requestFocus();
+                videoView.start();
+                break;
+
+            case IMAGE_ACTION_CODE:
+                Bundle extras =data.getExtras();
+                ((ImageView)findViewById(R.id.iv_camera)).setImageBitmap((Bitmap)extras.get("data"));
+                break;
+
+            default:
+                break;
+        }
+        }
+
+
+    }
